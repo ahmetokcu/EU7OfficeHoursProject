1366e0112b5f23f67c6077610dd6682af587ff2a
kavya tolety <ahmetokcu2020@gmail.com>
2022-02-05 00:08:55 +0100

    Example

---
+++ src/test/java/com/cybertek/javareviews/PracticeDay1/Light.java
@@ -0,0 +1,8 @@
+package com.cybertek.javareviews.PracticeDay1;
+
+public abstract class Light {
+
+    protected String location;  // why did I make it protected? : Only inheriting classes should be able to reach the field
+
+    public abstract void giveLight();
+}
---
+++ src/test/java/com/cybertek/javareviews/PracticeDay1/TrafficLightObjects.java
@@ -0,0 +1,34 @@
+package com.cybertek.javareviews.PracticeDay1;
+
+public class TrafficLightObjects {
+
+    public static void main(String[] args) {
+
+        TrafficLight trafficLight; // Object variable / reference to object
+
+        trafficLight = new TrafficLight("09:42 PM"); // new TrafficLight(); this statement holds the object
+
+        //  new TrafficLight().changeColor("yellow");
+
+        trafficLight.changeColor("red");
+
+        trafficLight.showColor();
+
+        trafficLight.changeColor("black");
+
+        TrafficLight trafficLight2 = new TrafficLight();
+        trafficLight2.changeColor("yellow");
+
+        System.out.println("trafficLight2 = " + trafficLight2.localTime);
+        System.out.println("TrafficLight.localTime = " + TrafficLight.localTime);
+        System.out.println("trafficLight = " + trafficLight.localTime);
+
+
+        trafficLight.showColor();
+        trafficLight2.showColor();
+
+        TrafficLight.showTime();
+
+    }
+
+}
