480ebb5b714ffd4fb4eef7ac39afd4394484d0fa
kavya tolety <ahmetokcu2020@gmail.com>
2022-02-19 00:21:18 +0100

    Examples

---
+++ src/test/java/com/cybertek/javaReviews/oopHomeworkSolutions/q13/ColorTest.java
@@ -0,0 +1,15 @@
+package com.cybertek.javaReviews.oopHomeworkSolutions.q13;
+
+public class ColorTest {
+    public static void main(String[] args) {
+        Color color = new Color();
+        System.out.println(color);
+
+        Color color1 = new Color(255,0,0);
+        System.out.println(color1);
+
+        Color color2 = new Color(0,255,255);
+        System.out.println(color2);
+    }
+
+}
